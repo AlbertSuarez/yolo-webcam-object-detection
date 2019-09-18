04257d6561bc44fe05aecf055b73deb9e6a89d2b
Albert Suarez <alsumo95@gmail.com>
2019-09-18 16:56:00 +0200

    Implemented project

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+MODEL_PATH = 'cfg/tiny-yolo-voc.cfg'
+WEIGHTS_PATH = 'cfg/tiny-yolo-voc.weights'
+MODEL_THRESHOLD = 0.2
+
+
+__all__ = [
+    'MODEL_PATH',
+    'WEIGHTS_PATH',
+    'MODEL_THRESHOLD'
+]
---
+++ src/__main__.py
@@ -0,0 +1,5 @@
+from src import yolo
+
+
+if __name__ == '__main__':
+    yolo.run()
